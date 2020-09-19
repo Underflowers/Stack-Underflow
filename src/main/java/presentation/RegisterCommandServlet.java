d7800aecb8da0be2a65fed02046b69367e6fc40c
Robin <r.demarta@outlook.com>
2020-09-19 16:54:30 +0200

    Fix var names

--- src/main/java/presentation/RegisterCommandServlet.java
+++ src/main/java/presentation/RegisterCommandServlet.java
@@ -9,10 +9,10 @@ import java.util.HashMap;
 @WebServlet(name = "RegisterCommandServlet", urlPatterns = "/registerCommand")
 public class RegisterCommandServlet  extends javax.servlet.http.HttpServlet {
 
-    private static HashMap<String, User> userMap = new HashMap<>(); // Temporary user "database"
+    private static HashMap<String, User> users = new HashMap<>(); // Temporary user "database"
 
-    public static int userCount() {
-        return userMap.size();
+    public static int usersCount() {
+        return users.size();
     }
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
@@ -22,10 +22,10 @@ public class RegisterCommandServlet  extends javax.servlet.http.HttpServlet {
                 .email(request.getParameter("email"))
                 .password(request.getParameter("password"))
                 .build();
-        userMap.put(request.getParameter("email"), newUser);
+        users.put(request.getParameter("email"), newUser);
 
         request.setAttribute("user", newUser);
-        response.sendRedirect("/stack-underflow/questions");
+        response.sendRedirect("/stack-underflow/register");
     }
 
 }
--- src/main/java/presentation/RegisterServlet.java
+++ src/main/java/presentation/RegisterServlet.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class RegisterServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.setAttribute("usersCount", RegisterCommandServlet.userCount());
+        request.setAttribute("usersCount", RegisterCommandServlet.usersCount());
         request.getRequestDispatcher("/WEB-INF/views/register.jsp").forward(request, response);
     }
 }
