5bd5c6296409053ba8af1480002b4197ffbad26d
Doran Kayoumi <dorankayoumi@gmail.com>
2020-09-28 23:23:45 +0200

    Refactor InMemory classes
    Moved common code in abstract class

--- src/main/java/underflowers/stackunderflow/infrastucture/persistence/memory/InMemoryUserRepository.java
+++ src/main/java/underflowers/stackunderflow/infrastucture/persistence/memory/InMemoryUserRepository.java
@@ -1,52 +1,17 @@
 package underflowers.stackunderflow.infrastucture.persistence.memory;
 
-import underflowers.stackunderflow.application.user.UsersQuery;
 import underflowers.stackunderflow.domain.user.IUserRepository;
 import underflowers.stackunderflow.domain.user.User;
 import underflowers.stackunderflow.domain.user.UserId;
 
 import java.util.Collection;
-import java.util.Map;
-import java.util.Optional;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
-public class InMemoryUserRepository implements IUserRepository {
-    private final Map<UserId, User> store = new ConcurrentHashMap<>();
-
-
+public class InMemoryUserRepository extends InMemoryRepository<User, UserId> implements IUserRepository {
     @Override
-    public Collection<User> find(UsersQuery query) {
-        // TODO when UsersQuery is better defined
-        return null;
-    }
-
-    @Override
-    public void save(User user) {
-        store.put(user.getId(), user);
-    }
-
-    @Override
-    public void remove(UserId userId) {
-        store.remove(userId);
-    }
-
-    @Override
-    public Optional<User> findById(UserId userId) {
-        User existingUser = store.get(userId);
-        if (existingUser == null) {
-            return Optional.empty();
-        }
-
-        User clonedUser = existingUser.toBuilder().build();
-        return Optional.of(clonedUser);
-
-    }
-
-    @Override
-    public Collection<User> findAll() {
-        return store.values().stream()
-                .map(user -> user.toBuilder().build())
+    public Collection<User> findByUsername(String username) {
+        return findAll().stream()
+                .filter(u -> u.getUsername().equals(username))
                 .collect(Collectors.toList());
     }
 }
