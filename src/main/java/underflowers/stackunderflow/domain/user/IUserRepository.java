65516a230571c8fce9d164426ebe6d7852428dd0
Doran Kayoumi <dorankayoumi@gmail.com>
2020-09-28 17:16:47 +0200

    Prepare domain for login and registration

--- src/main/java/underflowers/stackunderflow/domain/user/IUserRepository.java
+++ src/main/java/underflowers/stackunderflow/domain/user/IUserRepository.java
@@ -1,10 +1,9 @@
 package underflowers.stackunderflow.domain.user;
 
-import underflowers.stackunderflow.application.user.UsersQuery;
 import underflowers.stackunderflow.domain.IRepository;
 
 import java.util.Collection;
 
 public interface IUserRepository extends IRepository<User, UserId> {
-    public Collection<User> find(UsersQuery query);
+    public Collection<User> findByUsername(String username);
 }
--- src/main/java/underflowers/stackunderflow/domain/user/User.java
+++ src/main/java/underflowers/stackunderflow/domain/user/User.java
@@ -1,18 +1,58 @@
 package underflowers.stackunderflow.domain.user;
 
 import lombok.*;
+import underflowers.stackunderflow.domain.IEntity;
 import underflowers.stackunderflow.domain.question.QuestionId;
 
 @Data
 @Builder (toBuilder = true)
-public class User {
+public class User implements IEntity<User, UserId> {
 
     @Setter(AccessLevel.NONE)
-    @Builder.Default
-    private UserId id = new UserId();
+    private UserId id;
 
+    private String username;
     private String firstname;
     private String lastname;
     private String email;
+
+    @EqualsAndHashCode.Exclude
     private String password;
+
+    @Override
+    public User deepClone() {
+        return this.toBuilder().id(new UserId(id.asString())).build();
+    }
+
+    public static class UserBuilder {
+        public UserBuilder clearTextPassword(String clearTextPassword) {
+
+            if (clearTextPassword == null || clearTextPassword.isEmpty())
+                throw new java.lang.IllegalArgumentException("Password is mandatory!");
+
+            // TODO hash password
+            password = clearTextPassword;
+            return this;
+        }
+
+        public User build() {
+            if (id == null)
+                id = new UserId();
+
+            if (username == null || username.isEmpty())
+                throw new java.lang.IllegalArgumentException("Username is mandatory!");
+
+            if (firstname == null || firstname.isEmpty())
+                throw new java.lang.IllegalArgumentException("First name is mandatory!");
+
+            if (lastname == null || lastname.isEmpty())
+                throw new java.lang.IllegalArgumentException("Last name is mandatory!");
+
+            if (email == null || email.isEmpty())
+                throw new java.lang.IllegalArgumentException("Email is mandatory!");
+
+
+            return new User(id, username, email, firstname, lastname, password);
+        }
+    }
 }
