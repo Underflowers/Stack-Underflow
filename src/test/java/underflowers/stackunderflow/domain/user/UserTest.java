be6606bb6e8245e97cdc957f117bc504da0a04f7
Loïc Dessaules <loic.dessaules@gmail.com>
2020-10-14 11:31:16 +0200

    Add unit test for User class

--- src/test/java/underflowers/stackunderflow/domain/user/UserTest.java
+++ src/test/java/underflowers/stackunderflow/domain/user/UserTest.java
@@ -1,58 +1,79 @@
 package underflowers.stackunderflow.domain.user;
 
 import org.junit.jupiter.api.Test;
-import underflowers.stackunderflow.domain.question.Question;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
 
-    /*
-    @Test
-    void userCanAuthenticate() {
-        String clearTextPassword = "john";
-        User user = User.builder()
-                    .id(new UserId())
-                    .firstname("john")
-                    .lastname("doe")
-                    .email("john@doe.com")
-                    .clearTextPassword(clearTextPassword, clearTextPassword)
-                    .build();
-
-        assertTrue(user.authenticate(clearTextPassword));
-    }
-
     @Test
     void cannotCreateUserWithoutFirstname() {
-        String clearTextPassword = "john";
         assertThrows(IllegalArgumentException.class, () -> User.builder()
-                .id(new UserId())
                 .lastname("doe")
                 .email("john@doe.com")
-                .clearTextPassword(clearTextPassword, clearTextPassword)
+                .password("pass")
                 .build());
     }
 
     @Test
     void cannotCreateUserWithoutLastname() {
-        String clearTextPassword = "john";
         assertThrows(IllegalArgumentException.class, () -> User.builder()
-                .id(new UserId())
-                .firstname("John")
+                .firstname("john")
                 .email("john@doe.com")
-                .clearTextPassword(clearTextPassword, clearTextPassword)
+                .password("pass")
                 .build());
     }
 
     @Test
     void cannotCreateUserWithoutEmail() {
-        String clearTextPassword = "john";
         assertThrows(IllegalArgumentException.class, () -> User.builder()
-                .id(new UserId())
-                .firstname("John")
-                .lastname("Doe")
-                .clearTextPassword(clearTextPassword, clearTextPassword)
+                .firstname("john")
+                .lastname("doe")
+                .password("pass")
                 .build());
     }
-*/
+
+    @Test
+    void cannotCreateUserWithoutPassword() {
+        assertThrows(IllegalArgumentException.class, () -> User.builder()
+                .firstname("john")
+                .lastname("john")
+                .email("john@doe.com")
+                .build());
+    }
+
+    @Test
+    void canCreateUser() {
+        assertDoesNotThrow(() -> User.builder()
+                .firstname("john")
+                .lastname("doe")
+                .email("john@doe.com")
+                .clearTextPassword("john")
+                .build());
+    }
+
+    @Test
+    void cannotCreateUserWhithPasswordLengthMinusThan4() {
+        assertThrows(IllegalArgumentException.class, () -> User.builder()
+                .firstname("john")
+                .lastname("doe")
+                .email("john@doe.com")
+                .clearTextPassword("joe")
+                .build());
+    }
+
+    @Test
+    void userCanAuthenticate() {
+        String clearTextPassword = "john";
+        User user = User.builder()
+                .id(new UserId())
+                .firstname("john")
+                .lastname("doe")
+                .email("john@doe.com")
+                .clearTextPassword(clearTextPassword)
+                .build();
+
+        assertTrue(user.authenticate(clearTextPassword));
+    }
+
 }
