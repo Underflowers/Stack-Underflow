89e4f8f890000c4fd5a5c25c8a61ad6c25dff208
Thibaud Franchetti <5482364-franchetti@users.noreply.gitlab.com>
2021-01-15 20:23:27 +0100

    Resolve gamification URL at runtime

--- src/main/java/underflowers/stackunderflow/application/initialization/InitializationService.java
+++ src/main/java/underflowers/stackunderflow/application/initialization/InitializationService.java
@@ -12,5 +12,6 @@ public class InitializationService {
     @PostConstruct
     public void init() {
         Configuration.getDefaultApiClient().setApiKey(System.getenv("UNDERIFICATION_AUTH_TOKEN"));
+        Configuration.getDefaultApiClient().setBasePath(System.getenv("UNDERIFICATION_URL"));
     }
 }
